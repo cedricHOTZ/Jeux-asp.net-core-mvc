10f2d34a3297c8ce44718b6b3af2cf94882008a7
Cédric Hotz <36566958+cedricHOTZ@users.noreply.github.com>
2020-03-24 21:38:22 +0100

    cedric
    Mise en place du model Aventure et affichage moc.

--- Jeuxdontestlehero.Web.UI/Controllers/AventureController.cs
+++ Jeuxdontestlehero.Web.UI/Controllers/AventureController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Jeuxdontestlehero.Web.UI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Jeuxdontestlehero.Web.UI.Controllers
@@ -11,8 +12,20 @@ namespace Jeuxdontestlehero.Web.UI.Controllers
         public IActionResult Index()
         {
             this.ViewBag.Titre = "aventures";
-            this.ViewBag.MonTableau = new int[] { 1, 2, 3, 4 };
-            return View();
+            List<Aventure> maList = new List<Aventure>();
+            maList.Add(new Aventure()
+            {
+                Id = 1,
+                Titre="Ma première aventure"
+            });
+
+            maList.Add(new Aventure() { 
+
+
+           Id = 2,
+                Titre = "Ma seconde aventure"
+            }); ;
+            return View(maList);
         }
     }
 }
---
+++ Jeuxdontestlehero.Web.UI/Models/Aventure.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jeuxdontestlehero.Web.UI.Models
+{
+    public class Aventure
+    {
+        #region
+        public int Id { get; set; }
+        public string Titre { get; set; }
+        
+        #endregion
+    }
+}
