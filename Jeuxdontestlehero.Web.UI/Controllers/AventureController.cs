68993afd0e9ff36fbb64e003a2d526c2184c5c3c
Cédric Hotz <36566958+cedricHOTZ@users.noreply.github.com>
2020-03-24 15:13:43 +0100

    cedric
    Mise en place de la View aventure et du controller.

---
+++ Jeuxdontestlehero.Web.UI/Controllers/AventureController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jeuxdontestlehero.Web.UI.Controllers
+{
+    public class AventureController : Controller
+    {
+        public IActionResult Index()
+        {
+            this.ViewBag.Titre = "aventures";
+            this.ViewBag.MonTableau = new int[] { 1, 2, 3, 4 };
+            return View();
+        }
+    }
+}
---
+++ Jeuxdontestlehero.Web.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Jeuxdontestlehero.Web.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
